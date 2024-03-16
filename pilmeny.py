cb1abaf7804a8d2cc0fa7663b84c7d7fe30617cb
MarkusHage <127237872+MarkusHage@users.noreply.github.com>
2024-03-16 22:06:20 +0100

    fungerande meny

--- pilmeny.py
+++ pilmeny.py
@@ -1,60 +1,85 @@
 import os
 from getkey import getkey, keys
+import numpy as np
+import random
+
+Alternativ = ["Lägg till passagerare", "Skriv ut array", "Skriv ut sammanlagd ålder på passagerare", "Avsluta"]
+
+Passagerare = np.array([])
+
+def LäggTillPassagerare():
+    if len(Passagerare) < 25:
+        RandomÅlder = random.randint(5,99)
+        np.append(Passagerare, RandomÅlder)
+        print(f"En passagerare som är {RandomÅlder} år gammal hoppade på bussen. Totalt {len(Passagerare)} på bussen.")
+    else:
+        print("Tyvärr grabbar, inga slots lediga. Bussen är full.")
+    
+
+def SkrivUtArray():
+    if len(Passagerare) > 0:
+        print(Passagerare)
+    else:
+        print("Bussen är tom!")
+
+def SammanlagdÅlder():
+    if len(Passagerare) > 0:
+        print(sum(Passagerare))
+    else:
+        print("Bussen är tom!")  
+
+def Avsluta():
+    print("Avlutar programmet...")
+    exit()
 
 
 def meny():
-    
+
     Menyval = 0
-    
-    print("Första valet <--")
-    print("Andra valet")
-    print("Avsluta")
-    print()
-    print("Välj alternativ med piltangenterna")
-    
 
     while True:
+        
         key = getkey()
+        
+        match key:
+            case keys.UP:
+                if Menyval > 0:
+                    Menyval -= 1
+                    os.system('clear')
+                else:
+                    os.system('clear')
+            case keys.DOWN:
+                if Menyval < 2:
+                    Menyval += 1
+                    os.system('clear')
+                else:
+                    os.system('clear')
+            case keys.ENTER:
+                match Menyval:
+                    case 0:
+                        os.system('clear')
+                        LäggTillPassagerare()
+                    case 1:
+                        os.system('clear')
+                        SkrivUtArray()
+                    case 2:
+                        os.system('clear')
+                        Avsluta()
 
-        if key == keys.UP and Menyval > 0:
-            Menyval -= 1
-        elif key == keys.DOWN and Menyval < 2:
-            Menyval += 1
-        elif key == keys.ENTER:
-            if Menyval == 0:
-                print("Första valet")
-                #Första valet
-            elif Menyval == 1:
-                print("Andra valet")
-                #Andra valet
-            else:
-                exit()
-                #Avsluta
-        else:
-            buffer += key
-            print(buffer)
 
         if Menyval == 0:
             print("Första valet <--")
             print("Andra valet")
             print("Avsluta")
         elif Menyval == 1:
+            #os.system('clear')
             print("Första valet")
             print("Andra valet <--")
             print("Avsluta")
         elif Menyval == 2:
+            #os.system('clear')
             print("Första valet")
             print("Andra valet")
             print("Avsluta <--")
-
           
 meny()       
-
-"""
-if användare_input in meny_alternativ:
-    return användare_input
-else:
-    os.system('clear')
-    print("Ogiltigt val")
-    print()
-"""
